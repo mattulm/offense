73c896c1037fea872bb581f93ae50d891b7431f9
Matthew Ulm <mtju74@gmail.com>
2015-05-13 12:06:07 -0500

    2015-05-13
    2015-05-13

--- scripts/niktoscan.sh
+++ scripts/niktoscan.sh
@@ -10,16 +10,18 @@ git pull
 #
 # Run Some scans.
 cd program
-./nikto.pl -evasion 2 -Tuning 1 -no404 -useragent Mozilla -host $1 >> /home/$1.t1.nikto
+./nikto.pl -e 2 -Tuning 1 -C none -no404 -useragent Mozilla -Display 2 -host $1 >> /home/$1.t1.nikto
 sleep 15;
-./nikto.pl -evasion 4 -Tuning 2 -no404 -useragent Mozilla -host $1 >> /home/$1.t2.nikto
+./nikto.pl -e 4 -Tuning 2 -C none -no404 -useragent Mozilla -Display 2 -host $1 >> /home/$1.t2.nikto
 sleep 15;
-./nikto.pl -evasion 5 -Tuning 3 -no404 -useragent Mozilla -host $1 >> /home/$1.t3.nikto
+./nikto.pl -e 5 -Tuning 3 -C none -no404 -useragent Mozilla -Display 2 -host $1 >> /home/$1.t3.nikto
 sleep 15;
-./nikto.pl -evasion 3 -Tuning e -no404 -useragent Mozilla -host $1 >> /home/$1.te.nikto
+./nikto.pl -e B -Tuning e -C none -no404 -useragent Mozilla -Display 2 -host $1 >> /home/$1.te.nikto
 sleep 15;
-./nikto.pl -evasion 1 -Tuning 5 -no404 -useragent Mozilla -host $1 >> /home/$1.t5.nikto
+./nikto.pl -e 1 -Tuning 5 -C none -no404 -useragent Mozilla -Display 2 -host $1 >> /home/$1.t5.nikto
 sleep 15;
-./nikto.pl -evasion 8 -Tuning 7 -no404 -useragent Mozilla -host $1 >> /home/$1.t7.nikto
+./nikto.pl -e 8 -Tuning 7 -C none -no404 -useragent Mozilla -Display 2 -host $1 >> /home/$1.t7.nikto
+sleep 15;
+./nikto.pl -C all -e A -useragent Mozilla -IgnoreCode -host $1 >> /home/$1.t7.nikto
 
 
